1375b2b3ede83bea9031bf03b9bd37123fc9ee6e
anton <email@bk.ru>
2017-10-26 20:13:08 +0500

    написал вспомогательный клас для работы с сессией

---
+++ src/main/resources/main/helpers/SessionHelper.java
@@ -0,0 +1,28 @@
+package main.helpers;
+
+import javax.servlet.http.HttpSession;
+
+public class SessionHelper {
+    public final  HttpSession session;
+    public  SessionHelper(HttpSession session)
+    {
+        this.session = session;
+    }
+
+    public void setPage(String page) {
+        session.setAttribute("page", page);
+    }
+
+    public String getPage() {
+        return (String) session.getAttribute("page");
+    }
+
+    public void setPersonId(Integer id) {
+        session.setAttribute("id", id);
+    }
+
+    public Integer getPersonId() {
+        return (Integer) session.getAttribute("id");
+    }
+
+}
