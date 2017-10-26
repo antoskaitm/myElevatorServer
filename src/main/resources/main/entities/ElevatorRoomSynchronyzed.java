c3bbce54175341a2a1a41979175cf93207b35010
anton <email@bk.ru>
2017-10-27 01:33:06 +0500

    добавлен класс оболочка для синхронизации работы комнаты т.к. лифт эмулируется в одном потоке, но вызовы поступают от разных позже заменит текущюю комнату(не синхронизированную)

---
+++ src/main/resources/main/entities/ElevatorRoomSynchronyzed.java
@@ -0,0 +1,55 @@
+package main.entities;
+
+import java.io.Serializable;
+
+/**view for synchronization IElevatorRoom
+ *
+ */
+public class ElevatorRoomSynchronyzed implements IElevatorRoom, IElevatorAutomateble , Serializable {
+
+    private IElevatorRoom elevatorRoom;
+
+    public <T extends IElevatorRoom& IElevatorAutomateble & Serializable> ElevatorRoomSynchronyzed(T elevatorRoom) {
+        this.elevatorRoom = elevatorRoom;
+    }
+
+    @Override
+    public IElevatorAutomate getElevatorAutomate() {
+        return ((IElevatorAutomateble)elevatorRoom).getElevatorAutomate();
+    }
+
+    @Override
+    public synchronized Integer callElevator(int floor) {
+        return elevatorRoom.callElevator(floor);
+    }
+
+    @Override
+    public Integer getCurrentFloor() {
+        return elevatorRoom.getCurrentFloor();
+    }
+
+    @Override
+    public synchronized Boolean sendElevator(int floor, int personId) {
+        return elevatorRoom.sendElevator(floor,personId);
+    }
+
+    @Override
+    public boolean isInElevator(Integer personId) {
+        return elevatorRoom.isSendElevator(personId);
+    }
+
+    @Override
+    public boolean isCallElevator(Integer personId) {
+        return elevatorRoom.isCallElevator(personId);
+    }
+
+    @Override
+    public boolean isSendElevator(Integer personId) {
+        return elevatorRoom.isSendElevator(personId);
+    }
+
+    @Override
+    public String getPersonCondition(Integer personId) {
+        return elevatorRoom.getPersonCondition(personId);
+    }
+}
