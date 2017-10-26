f876c919c613175002fc1d3afe335da6009e9820
anton <email@bk.ru>
2017-10-26 15:22:55 +0500

    Revert "прекинул интерфейсы в отдельный пакет"
    This reverts commit 94af91c3c9cc3480d8c3287579ebd3e09c2215e5.
    
    # Conflicts:
    #	src/main/resources/main/entities/IElevatorAutomateble.java

--- src/main/resources/main/entities/IElevatorAutomateble.java
+++ src/main/resources/main/entities/IElevatorAutomateble.java
@@ -1,37 +1,5 @@
 package main.entities;
 
-import main.entities.events.Action;
-
 interface IElevatorAutomateble {
     public IElevatorAutomate getElevatorAutomate();
-
-    interface IElevaterRoom {
-        Integer callElevator(int floor);
-
-        Integer getCurrentFloor();
-
-        Boolean SendElevator(int floor, int personId);
-    }
-
-    interface IElevatorAutomate {
-        void stop();
-
-        Boolean stopNextFloor();
-
-        void changeCurrentFloor();
-
-        Boolean canMove();
-
-        void onStop(Action event);
-    }
-
-    interface IElevatorAutomateble {
-        public IElevatorAutomate getElevatorAutomate();
-    }
-
-    interface IElevatorUi {
-        Integer getCurrentFloor();
-
-        Boolean callup(Integer floor);
-    }
 }
