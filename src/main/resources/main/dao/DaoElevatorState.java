f54fe83f21e7f5dd3551ba991eedb7683bc8a1e8
anton <email@bk.ru>
2017-10-26 21:46:01 +0500

    добавил файл лифт и работу с ним

--- src/main/resources/main/dao/DaoElevatorState.java
+++ src/main/resources/main/dao/DaoElevatorState.java
@@ -2,13 +2,22 @@ package main.dao;
 
 import main.entities.IElevaterRoom;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+
 public class DaoElevatorState {
+    private File file = new File("/WEB-INF/lift.txt");
 
     public IElevaterRoom getElevator() {
         return null;
     }
 
-    public void saveElevator(IElevaterRoom room) {
+    public void saveElevator(IElevaterRoom room) throws IOException {
+        try(ObjectOutputStream stream = new ObjectOutputStream(new FileOutputStream(file)))
+        {
 
+        }
     }
 }
