48bda24b014ef85d4990a00db26ce902c22ae8c0
anton <email@bk.ru>
2017-10-26 21:11:54 +0500

    создал клас для сохранения комнаты лифта

---
+++ src/main/resources/main/dao/DaoElevatorState.java
@@ -0,0 +1,14 @@
+package main.dao;
+
+import main.entities.IElevaterRoom;
+
+public class DaoElevatorState {
+
+    public IElevaterRoom getElevator() {
+        return null;
+    }
+
+    public void saveElevator(IElevaterRoom room) {
+
+    }
+}
