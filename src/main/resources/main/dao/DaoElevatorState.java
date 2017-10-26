3c4c7ad9448284705162a07c7f11fb7f6317dde4
anton <email@bk.ru>
2017-10-26 21:50:48 +0500

    разработал класс для чтения и записи состояния

--- src/main/resources/main/dao/DaoElevatorState.java
+++ src/main/resources/main/dao/DaoElevatorState.java
@@ -1,23 +1,24 @@
 package main.dao;
 
+import com.sun.xml.internal.bind.v2.runtime.output.XMLStreamWriterOutput;
 import main.entities.IElevaterRoom;
 
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.ObjectOutputStream;
+import java.io.*;
 
 public class DaoElevatorState {
     private File file = new File("/WEB-INF/lift.txt");
 
-    public IElevaterRoom getElevator() {
-        return null;
+    public IElevaterRoom getElevator() throws IOException, ClassNotFoundException {
+        try(ObjectInputStream stream = new ObjectInputStream(new FileInputStream(file)))
+        {
+            return (IElevaterRoom)stream.readObject();
+        }
     }
 
     public void saveElevator(IElevaterRoom room) throws IOException {
         try(ObjectOutputStream stream = new ObjectOutputStream(new FileOutputStream(file)))
         {
-
+            stream.writeObject(room);
         }
     }
 }
