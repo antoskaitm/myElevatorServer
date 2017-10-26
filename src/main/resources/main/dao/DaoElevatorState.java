92861ae6f5c65875ca95222ff00582da74649a40
anton <email@bk.ru>
2017-10-26 23:17:47 +0500

    теперь класс сохраняет

--- src/main/resources/main/dao/DaoElevatorState.java
+++ src/main/resources/main/dao/DaoElevatorState.java
@@ -1,23 +1,41 @@
 package main.dao;
 
-import com.sun.xml.internal.bind.v2.runtime.output.XMLStreamWriterOutput;
 import main.entities.IElevaterRoom;
+import java.io.File;
+import java.io.UnsupportedEncodingException;
+import java.net.URL;
+import java.net.URLDecoder;
+import java.nio.charset.Charset;
 
 import java.io.*;
+import java.nio.file.Paths;
 
 public class DaoElevatorState {
-    private File file = new File("/WEB-INF/lift.txt");
+    private File file;
 
-    public IElevaterRoom getElevator() throws IOException, ClassNotFoundException {
-        try(ObjectInputStream stream = new ObjectInputStream(new FileInputStream(file)))
-        {
-            return (IElevaterRoom)stream.readObject();
+    public DaoElevatorState() throws IOException {
+        file = CurrentDir();
+    }
+
+    public synchronized IElevaterRoom getElevator() throws IOException, ClassNotFoundException {
+        try (ObjectInputStream stream = new ObjectInputStream(new FileInputStream(file))) {
+            return (IElevaterRoom) stream.readObject();
         }
     }
 
-    public void saveElevator(IElevaterRoom room) throws IOException {
-        try(ObjectOutputStream stream = new ObjectOutputStream(new FileOutputStream(file)))
-        {
+    private static File CurrentDir() throws IOException {
+        String path = DaoElevatorState.class.getProtectionDomain().getCodeSource().getLocation().getFile().substring(1);
+        Integer index = path.lastIndexOf("classes");
+        path = path.subSequence(0, index).toString();
+        File file = Paths.get(path, "lift").toFile();
+        if (!file.exists()) {
+            file.createNewFile();
+        }
+        return file;
+    }
+
+    public synchronized void saveElevator(IElevaterRoom room) throws IOException {
+        try (ObjectOutputStream stream = new ObjectOutputStream(new FileOutputStream(file))) {
             stream.writeObject(room);
         }
     }
