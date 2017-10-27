9309853f5be3f86353cabe1b46a0703bdf74338e
anton <email@bk.ru>
2017-10-27 23:58:15 +0500

    создал интерфейс состояния человека

--- src/main/resources/main/entities/PersonCondition.java
+++ src/main/resources/main/entities/PersonCondition.java
@@ -1,19 +1,20 @@
 package main.entities;
 
-public class PersonCondition {
+import main.entities.interfaces.IPersonCondition;
+
+public class PersonCondition implements IPersonCondition {
     private final String message;
 
-    private PersonCondition(String message)
-    {
+    private PersonCondition(String message) {
         this.message = message;
     }
+
     public static PersonCondition DIDNOT_CALL_ELEVATOR = new PersonCondition("Person didn't call elevator room");
     public static PersonCondition CALLED_ELEVATOR = new PersonCondition("Person wait to into elevator room");
     public static PersonCondition STAND_IN_ELEVATOR = new PersonCondition("Person stand in elevator room");
     public static PersonCondition SENDED_ELEVATOR = new PersonCondition("Person wait to out from elevator room");
 
-    public String getMessage()
-    {
+    public String getMessage() {
         return message;
     }
 }
---
+++ src/main/resources/main/entities/interfaces/IPersonCondition.java
@@ -0,0 +1,5 @@
+package main.entities.interfaces;
+
+public interface IPersonCondition {
+    String getMessage();
+}
