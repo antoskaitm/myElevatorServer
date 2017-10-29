d676b645d005255196de9129c6d24ec2969f56c3
anton <email@bk.ru>
2017-10-29 14:08:44 +0400

    добавил классы описывающие состояния, для ответа на клиент

---
+++ src/main/resources/main/emulator/panel/PersonInfo.java
@@ -0,0 +1,31 @@
+package main.emulator.panel;
+
+public class PersonInfo {
+    private String personConditionMessage;
+    private Integer personId;
+    private String errorMessage;
+
+    public String getPersonConditionMessage() {
+        return personConditionMessage;
+    }
+
+    public void setPersonConditionMessage(String personConditionMessage) {
+        this.personConditionMessage = personConditionMessage;
+    }
+
+    public Integer getPersonId() {
+        return personId;
+    }
+
+    public void setPersonId(Integer personId) {
+        this.personId = personId;
+    }
+
+    public String getErrorMessage() {
+        return errorMessage;
+    }
+
+    public void setErrorMessage(String errorMessage) {
+        this.errorMessage = errorMessage;
+    }
+}
---
+++ src/main/resources/main/emulator/panel/ServerInfo.java
@@ -0,0 +1,31 @@
+package main.emulator.panel;
+
+public class ServerInfo {
+    private Integer currentFloor;
+    private Integer lastFloor;
+    private Integer groundFloor;
+
+    public Integer getCurrentFloor() {
+        return currentFloor;
+    }
+
+    public void setCurrentFloor(Integer currentFloor) {
+        this.currentFloor = currentFloor;
+    }
+
+    public Integer getLastFloor() {
+        return lastFloor;
+    }
+
+    public void setLastFloor(Integer lastFloor) {
+        this.lastFloor = lastFloor;
+    }
+
+    public Integer getGroundFloor() {
+        return groundFloor;
+    }
+
+    public void setGroundFloor(Integer groundFloor) {
+        this.groundFloor = groundFloor;
+    }
+}
