4527519d694913020d38736d40f29f8a2a7394e8
anton <email@bk.ru>
2017-10-30 20:45:32 +0400

    добавил абстрактное здание с лифтами

---
+++ src/main/resources/main/entities/primitive/BuildingWithVariousFloors.java
@@ -0,0 +1,53 @@
+package main.entities.primitive;
+
+import main.entities.interfaces.primitive.IFloorsRange;
+import main.entities.interfaces.primitive.IPersonElevator;
+import main.entities.primitive.abstractclass.AbstractBuilding;
+
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.util.Arrays;
+
+public class BuildingWithVariousFloors extends AbstractBuilding {
+	private static final long serialVersionUID = 0;
+	private Integer[] floorsHeights;
+
+	public BuildingWithVariousFloors(IFloorsRange floorsRange, IPersonElevator... elevators) {
+		super(floorsRange, elevators);
+		if (floorsHeights.length != floorsRange.getFloorCount()) {
+			throw new IllegalArgumentException("Wrong floor count");
+		}
+		this.floorsHeights = Arrays.copyOf(floorsHeights, floorsHeights.length);
+		checkFloorHeights();
+	}
+
+	@Override
+	public Integer getFloorHeight(Integer floor) {
+		return floorsHeights[floor];
+	}
+
+	private void checkFloorHeights() {
+		Integer minFloorHeight = 3;
+		for (Integer floorHeight : this.floorsHeights) {
+			if (floorHeight < minFloorHeight) {
+				throw new IllegalArgumentException("Value of floor height  must be more than "
+						+ minFloorHeight);
+			}
+		}
+	}
+
+	@Override
+	protected void readObject(ObjectInputStream stream) throws IOException, ClassNotFoundException {
+		super.readObject(stream);
+		long serialVersionUID = stream.readLong();
+		floorsHeights = (Integer[]) stream.readObject();
+	}
+
+	@Override
+	protected void writeObject(ObjectOutputStream stream) throws IOException {
+		super.writeObject(stream);
+		stream.writeLong(serialVersionUID);
+		stream.writeObject(floorsHeights);
+	}
+}
---
+++ src/main/resources/main/entities/primitive/abstractclass/AbstractBuilding.java
@@ -0,0 +1,52 @@
+package main.entities.primitive.abstractclass;
+
+import main.entities.interfaces.primitive.IBuilding;
+import main.entities.interfaces.primitive.IFloorsRange;
+import main.entities.interfaces.primitive.IPersonElevator;
+
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+
+public abstract class AbstractBuilding implements IBuilding{
+	private static final long serialVersionUID = 0;
+	private IFloorsRange floorsRange;
+	private IPersonElevator[] elevators;
+
+	public AbstractBuilding(IFloorsRange floorsRange, IPersonElevator... elevators) {
+		this.elevators = elevators;
+		this.floorsRange = floorsRange;
+		checkFloorsRanges(elevators);
+		this.elevators = elevators;
+	}
+
+	@Override
+	public IFloorsRange getFloorsRange() {
+		return this.floorsRange;
+	}
+
+	@Override
+	public IPersonElevator getElevator(Integer elevatorNumber) {
+		return elevators[elevatorNumber];
+	}
+
+	private void checkFloorsRanges(IPersonElevator[] elevators) {
+		for (int index = 0; index < elevators.length; index++) {
+			if (!floorsRange.contains(elevators[index].getFloorsRange())) {
+				throw new IllegalStateException("Elevator by index " + index + " go out of building floors range");
+			}
+		}
+	}
+
+	protected void readObject(ObjectInputStream stream) throws IOException, ClassNotFoundException {
+		long serialVersionUID = stream.readLong();
+		floorsRange = (IFloorsRange) stream.readObject();
+		elevators = (IPersonElevator[]) stream.readObject();
+	}
+
+	protected void writeObject(ObjectOutputStream stream) throws IOException {
+		stream.writeLong(serialVersionUID);
+		stream.writeObject(floorsRange);
+		stream.writeObject(elevators);
+	}
+}
